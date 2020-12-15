f5b3741a1c40acbc1899803375ad29af9075be3a
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-12-15 21:36:55 +0900

    Setting Properties

--- src/main/java/com/mdjuniooor/shop/mall/MallApplication.java
+++ src/main/java/com/mdjuniooor/shop/mall/MallApplication.java
@@ -2,7 +2,9 @@ package com.mdjuniooor.shop.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Actuate Auditing ( auto setting for update & create time)
 @SpringBootApplication
 public class MallApplication {
 
